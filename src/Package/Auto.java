4053e0a5d62e2c0fdfed468e3435ed81185ec910
locatelli.21131 <locatelli.federico.studente@itispaleocapa.it>
2024-09-30 12:54:08 +0200

    easy

--- src/Package/Auto.java
+++ src/Package/Auto.java
@@ -1,5 +1,35 @@
 package Package;
 
 public class Auto {
+    private String modello;
+    private String marca;
+    private String prezzo;
+
+    public Auto (String mrc, String mdl, String prz){
+        modello = mdl;
+        marca = mrc;
+        prezzo = prz;
+    }
+
+    public void setModello (String mdl){
+        modello = mdl;
+    }
+    public void setMarca (String mrc){
+        marca = mrc;
+    }
+    public void setPrezzo (String prz){
+        prezzo = prz;
+    }
+
+    public String getModello (){
+        return modello;
+    }
+    public String getMarca (){
+        return marca;
+    }
+    public String getPrezzo (){
+        return prezzo;
+    }
+
 
 }
--- src/Package/Main.java
+++ src/Package/Main.java
@@ -1,4 +1,20 @@
 package Package;
+import java.util.Scanner;
 
 public class Main {
+    public static void main (String [] args) {
+        Scanner in = new Scanner(System.in);
+        Auto a1 = new Auto("audi", "r8", "250.000");
+        Auto a2 = new Auto("fiat", "panda", "10.000");
+        Auto a3 = new Auto("mcLaren", "senna", "550.000");
+        Auto a4 = new Auto("opel", "corsa", "20.000");
+
+        System.out.println(a1.getMarca()+ " " + a1.getModello() + " " + a1.getPrezzo());
+        System.out.println(a2.getMarca()+ " " + a2.getModello() + " " + a2.getPrezzo());
+        System.out.println(a3.getMarca()+ " " + a3.getModello() + " " + a3.getPrezzo());
+        System.out.println(a4.getMarca()+ " " + a4.getModello() + " " + a4.getPrezzo());
+
+
+
+    }
 }
